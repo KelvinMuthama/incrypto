3cf98ea916bc3b804a4c9d6a50049891eb016657
kelvinmuthama <kelvinkamau387@gmail.com>
2022-05-19 07:39:56 +0300

    Create App file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./index.css";
