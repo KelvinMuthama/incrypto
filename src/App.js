cc5b2af8a12e5501a61358a7a1c0fcb72461226c
kelvinmuthama <kelvinkamau387@gmail.com>
2022-05-19 07:40:39 +0300

    Add app jsx and routing with router dom

--- src/App.js
+++ src/App.js
@@ -11,3 +11,49 @@ import {
   News,
 } from "./components";
 import "./index.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+            </Routes>
+            <Routes>
+              <Route path="/exchanges" element={<Exchanges />} />
+            </Routes>
+            <Routes>
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            </Routes>
+            <Routes>
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+            </Routes>
+            <Routes>
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            In-Crypto <br /> All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
