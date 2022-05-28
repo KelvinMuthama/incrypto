3da94dad689eef33e15a899bddfd293ac6030cf2
kelvinmuthama <kelvinkamau387@gmail.com>
2022-05-28 06:22:45 +0300

    Create line chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,11 +1,67 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
 import { Col, Row, Typography } from "antd";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend
+);
 
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
   console.log(coinHistory);
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(
+      new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
+    );
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
   return (
     <>
       <Row className="chart-header">
@@ -14,13 +70,14 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
         </Title>
         <Col className="price-container">
           <Title level={5} className="price-change">
-            {coinHistory?.data?.change}%
+            {coinHistory?.data?.change} %
           </Title>
           <Title level={5} className="current-price">
             Current {coinName} Price: $ {currentPrice}
           </Title>
         </Col>
       </Row>
+      <Line data={data} options={options} />
     </>
   );
 };
